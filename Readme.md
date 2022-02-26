3315f882a5af0a4b5cacfcdc95968f9ff76a9640
muracz <marcin@uracz.pl>
2022-02-26 23:02:19 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,111 @@
+# Python Automic ServiceManagerDialog
+
+A CLI GUI wrapper for the Automic [ucybsmcl](https://docs.automic.com/documentation/webhelp/english/AA/21.0/DOCU/21.0/Automic%20Automation%20Guides/Content/ServiceManager/ServiceManager_CLI.htm)
+
+
+```
+Host: localhost:8871			Phrase:  Automic
+                                       Current time: 26-02-2022 22:40:43
+------------------------------------------------------------------------
+|  # |              Process Name |   |        PID |          Timestamp | 
+------------------------------------------------------------------------
+|  1 | AUTOMIC-DATABASE          | R |      70771 | 2022-02-25 - 11:51 | 
+|  2 | WP1                       | R |     126410 | 2022-02-25 - 23:12 | 
+|  3 | CP1                       | R |     126431 | 2022-02-25 - 23:13 | 
+|  4 | CP2                       | R |     126451 | 2022-02-25 - 23:13 | 
+|  5 | JWP                       | R |     126474 | 2022-02-25 - 23:13 | 
+|  6 | JCP                       | R |     126527 | 2022-02-25 - 23:13 | 
+|  7 | REST                      | R |     126586 | 2022-02-25 - 23:13 | 
+|  8 | AWI                       | R |      70976 | 2022-02-25 - 11:51 | 
+|  9 | WP2                       | R |     126760 | 2022-02-25 - 23:14 | 
+| 10 | WP3                       | R |     139030 | 2022-02-26 - 22:12 | 
+| 11 | WP4                       | S |            |                    | 
+| 12 | WP5                       | S |            |                    | 
+| 13 | DATABASE-AGENT01          | S |            |                    | 
+| 14 | DATABASE-SERVICE01        | S |            |                    | 
+| 15 | UNIX01                    | S |            |                    | 
+| 16 | WEBSERVICESOAP01          | R |      71148 | 2022-02-25 - 11:52 | 
+| 17 | JMS01                     | R |      71147 | 2022-02-25 - 11:52 | 
+| 18 | FTPAGENT01                | R |     127583 | 2022-02-25 - 23:17 | 
+| 19 | WEBSERVICEREST01          | R |      71240 | 2022-02-25 - 11:52 | 
+| 20 | RULE-ENGINE               | R |      71332 | 2022-02-25 - 11:52 | 
+| 21 | ANALYTICS-BACKEND         | R |      72132 | 2022-02-25 - 11:52 | 
+| 22 | STREAMING-PLATFORM        | R |      72214 | 2022-02-25 - 11:52 | 
+
+Actions: R - restart, S - start, K - stop
+         KA - stop abnormally , KS - shutdown 
+         Q - quit, RE - refresh
+| R
+```
+
+
+## Description
+
+This is an attempt to make the interaction with ServiceManager on Unix / commandline based interfaces less painful.
+
+## Getting Started
+
+### Dependencies
+
+* Python3 with standard libraries, no external dependecies used
+* Any version of ucybsmcl
+
+### Installing
+
+* git clone / download
+
+### Usage
+
+The script can be started in two modes
+
+#### With configuration file as parameter
+
+In this mode the script will process the data in the configuration file provided and let you choose which system you want to configure. 
+
+The format of the configuration file is following:
+
+```
+{ "connections": [
+  {
+    "name": "Test_V12.3",
+    "host": "localhost",
+    "port": "8872",
+    "phrase": "Automic1",
+    "pass": false,
+    "smgrclPath": "/app/Automic_12.3/Automation.Platform/ServiceManager/bin/ucybsmcl"
+    },
+{
+    "name": "Test_V21",
+    "host": "localhost",
+    "port": "8871",
+    "phrase": "Automic",
+    "pass": false,
+    "smgrclPath": "/app/Automic/Automation.Platform/ServiceManager/bin/ucybsmcl"
+}
+]}
+```
+You can define as many connections as you like
+
+
+
+
+
+
+
+
+
+#### Without configuration - interactive mode
+
+In this mode the script will ask you ( of use env variables ) to establish the connection to ServiceManager
+
+
+
+
+## Authors
+
+Marcin Uracz 
+
+## Version History
+
+* 1.0
+    * Initial Release
---
+++ pasdi.py
@@ -0,0 +1,244 @@
+#!/usr/bin/python3
+# Automic Service Manager Wrapper 
+# Created by Marcin Uracz 
+# Github: 
+# 
+
+
+import os, sys, subprocess, re, time, json, datetime, select
+
+# // TODo
+#  - set_data action for smgr > 12.3
+
+
+#  Constants / config 
+
+def getConfigJSON(file):
+    ## Read the config file
+    f = open(file)
+    config = json.load(f)
+    f.close()
+
+    if len(config['connections']) > 1:
+        c = 0 
+        print("Available configurations")
+        for conn in config['connections']:
+            print("%2d - %s" %( c, conn['name']))
+            c += 1
+
+        connID = input("Choose the config: ")
+        connID = int(connID)
+    else:
+        connID = 0
+
+
+    smgrPath = config['connections'][connID]['smgrclPath']
+    smgrPort = config['connections'][connID]['port']
+    smgrHost = config['connections'][connID]['host']
+    smgrPhrase = config['connections'][connID]['phrase']
+    if config['connections'][connID]['pass']:
+        smgrPass =  input("Enter the ServiceManager Pasword: ")
+    else:  
+        smgrPass = ""
+    
+    return smgrPath, smgrPort, smgrHost, smgrPhrase, smgrPass
+
+def getConfigInput():
+    
+    smgrPath = input("Path to ucybsmcl. Leave empty to use env variable $AUTOMIC_SMCL:  ") or os.getenv('AUTOMIC_SMCL')
+    smgrHost = input("Hostame: ")
+    smgrPort = input("ServiceManager port. Leave empty to use env variable $AUTOMIC_SMPORT:  ") or os.getenv('AUTOMIC_SMPORT')
+    smgrPhrase = input("Phrase. Leave empty to use env variable $AUTOMIC_PHRASE:  ") or os.getenv('AUTOMIC_SMCL')
+    smgrPass = input("Password. Leave empty if no password is configured ") 
+
+    return smgrPath, smgrPort, smgrHost, smgrPhrase, smgrPass
+
+# Get the config     
+try:
+    smgrPath, smgrPort, smgrHost, smgrPhrase, smgrPass = getConfigJSON(sys.argv[1])
+except: 
+    smgrPath, smgrPort, smgrHost, smgrPhrase, smgrPass = getConfigInput()
+
+
+
+# We need the port always when set so why do it over and over again
+if smgrPort is not None:
+    smgrHost = smgrHost + ":" + smgrPort
+
+## Build Preliminary argList
+smgrArgs = [smgrPath,'-h', smgrHost, '-n', smgrPhrase, '-p', smgrPass]
+        
+
+
+# Prepare env 
+os.environ['LD_LIBRARY_PATH'] = smgrPath.replace("ucybsmcl", "") 
+
+# Let's have some colours
+class bcolors:
+    OK = '\033[92m' #GREEN
+    WARNING = '\033[93m' #YELLOW
+    FAIL = '\033[91m' #RED
+    RESET = '\033[0m' #RESET COLOR
+
+# -------------------
+
+def clrScreen():
+    result = subprocess.run("clear")
+
+def getVersion():
+    result = subprocess.run([smgrPath, '-v'], stdout=subprocess.PIPE)
+
+    try:
+        Version = re.search('([0-9.-]+)\+', result.stdout.decode("utf-8")).group(1)
+        return Version
+    except AttributeError:
+        print('Error: Could not find version')
+        return None
+
+def getProcessList():
+    smgrArgs.append("-c")
+    smgrArgs.append("GET_PROCESS_LIST")
+
+    result = subprocess.run(smgrArgs, stdout=subprocess.PIPE)
+
+    if result.returncode > 0:
+        print(result.stdout.decode("utf-8"))
+        quit(result.returncode)
+
+    c = 1
+    procList = {}
+    # Populate the dict
+    for line in result.stdout.decode("utf-8").splitlines():
+       procList[c] = line.replace("\"","").split(" ")
+       c += 1
+
+    # Header 
+    clrScreen()
+    print()
+    print("Host: %s\t\t\tPhrase:  %s"  % (smgrHost, smgrPhrase)) 
+    print("Current time: %s".rjust(55)  % (datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S"))) 
+    print('-' * 72)
+    print("| %2s | %25s | %1s | %10s | %18s | " % ("#", "Process Name", "", "PID", "Timestamp"))
+    print('-' * 72)
+    ## Print  
+    for c in procList:
+        # Make it pretty
+        procName = procList[c][0].ljust(25)
+        procStatus = procList[c][1]
+        try:
+            procPID = procList[c][2]
+        except IndexError:
+            procPID = ''
+
+        try:
+            procTimestamp = procList[c][3] + " - " + procList[c][4]
+        except IndexError:
+            procTimestamp  = ''
+
+    # Add the colours
+        if procStatus == "R":
+            procStatus = bcolors.OK + procStatus + bcolors.RESET
+        else:
+            procStatus = bcolors.FAIL + procStatus + bcolors.RESET
+
+        print("| %2d | %25s | %3s | %10s | %18s | " % (c, procName,procStatus, procPID,procTimestamp))
+    return procList
+
+def stopProcess(ProcName,Mode=None):
+
+    # Add the appropriate paramters
+    smgrArgs.append("-c")
+    smgrArgs.append("STOP_PROCESS")
+    smgrArgs.append("-s")
+    smgrArgs.append(ProcName)
+    if Mode is not None:
+        smgrArgs.append("-m")
+        smgrArgs.append(Mode)
+    smgrArgs.append("-s")
+    smgrArgs.append(ProcName)
+    result = subprocess.run(smgrArgs, stdout=subprocess.PIPE)
+    if result.returncode > 0:
+        print(result.stdout.decode("utf-8"))
+
+def startProcess(ProcName):
+    print("Starting %s" % ProcName) 
+
+    # Add the appropriate paramters
+    smgrArgs.append("-c")
+    smgrArgs.append("START_PROCESS")
+    smgrArgs.append("-s")
+    smgrArgs.append(ProcName)
+
+    result = subprocess.run(smgrArgs, stdout=subprocess.PIPE)
+    if result.returncode > 0:
+        print(result.stdout.decode("utf-8"))
+
+def restartProcess(ProcName):
+    stopProcess(ProcName)
+    time.sleep(2)
+    startProcess(ProcName)
+
+def commitAction(a ,p):
+    if a == "R":
+        restartProcess(p)
+    elif a == "K":
+        stopProcess(p)
+    elif a == "KA":
+        stopProcess(p,"A")
+    elif a == "KS":
+        stopProcess(p,"S")
+    else:
+        startProcess(p)
+
+
+# Validation functions
+def validateCommit(inputCommit):
+    if inputCommit.upper() == "Y":
+        return True
+
+def validateNumber(inputNumber, procList):
+    while inputNumber not in range(1, len(procList)):
+        inputNumber = int(input("Invalind number, try again: "))
+    
+    return inputNumber
+
+def validateAction(inputAction):
+    while inputAction not in ("K","KA","KS","R","S","Q","RE"):
+        inputAction= input("Invalind action, try again ").upper()
+    
+    return inputAction
+
+#Version = getVersion()
+# print(Version)
+while True: 
+    commit = False
+    procList = getProcessList()
+
+    print()
+    print("Actions: R - restart, S - start, K - stop" ) 
+    print("         KA - stop abnormally , KS - shutdown ")
+    print("         Q - quit, RE - refresh")
+
+    # Timeout after 10 seconds a.k.a auto-refresh
+    i, o, e = select.select( [sys.stdin], [], [], 10 )
+
+    if i:
+        inputAction = validateAction(sys.stdin.readline().strip().upper())
+    else:
+        continue
+
+    if inputAction == "Q":
+        break
+    if inputAction == "RE":
+        continue
+
+    inputNumber = validateNumber(int(input("Which process number? ")),procList)
+    
+    print("Action: %s on %s. " % (inputAction, procList[inputNumber][0]))
+    commit = validateCommit(input("Commit Y/N ? "))
+
+    if commit:
+        commitAction(inputAction, procList[inputNumber][0])
+
+
+        
